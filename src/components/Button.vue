85aa70d47f9e5ee8e3d83f5a18ea61781c5d57d9
Gabriel Mule <gabemule@gmail.com>
2018-09-02 00:42:48 -0300

    Adding hit-button component and story

---
+++ src/components/Button.vue
@@ -0,0 +1,57 @@
+<template>
+  <button
+    @click="$emit('click')"
+    class="hit-button"
+    :class="[
+      size && '-'+size ,
+      variant && '-'+variant,
+      disabled && '-disabled'
+    ]"
+    :disabled="disabled"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: "md",
+      validator(value) {
+        return ["sm", "md", "lg"].indexOf(value) !== -1;
+      }
+    },
+    variant: {
+      type: String,
+      default: "primary",
+      validator(value) {
+        return (
+          ["primary", "primary-ghost", "secondary", "secondary-ghost"].indexOf(
+            value
+          ) !== -1
+        );
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.hit-button {
+  &.-sm {
+    padding: 5px;
+  }
+  &.-md {
+    padding: 7.5px;
+  }
+  &.-lg {
+    padding: 10px;
+  }
+}
+</style>
---
+++ src/stories/button.stories.js
@@ -0,0 +1,101 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { withInfo } from "storybook-addon-vue-info/lib/index";
+import { withOptions } from '@storybook/addon-options';
+import { withKnobs, text, select, boolean} from "@storybook/addon-knobs/vue";
+
+import Components from "./../components/index";
+
+const TextButton = "Button Text";
+const TypeButton = "Variant";
+const SizeButton = "Size";
+const optionsVariant = {
+  Primary: "primary",
+  "Primary Inverse": "primary-inverse",
+  Secondary: "Secondary",
+  "Secondary Inverse": "secondary-inverse",
+  Back: "back"
+};
+const optionsSize = {
+  Small: "sm",
+  Medium: "md",
+  Large: "lg"
+};
+
+storiesOf("Hit Button", module)
+  .addDecorator(withKnobs)
+  .add(
+    "Customizable Button",
+    withInfo({})(() => ({
+      components: { ...Components },
+      template: `
+        <hit-button 
+          @click="action"
+          size="${select(SizeButton, optionsSize, "sm")}" 
+          variant="${select(TypeButton, optionsVariant, "primary")}"
+          :disabled="${boolean('Disabled', false)}"
+        >
+          ${text(TextButton, "Button Text")}
+        </hit-button>
+      `,
+      methods: { action: action("clicked") },
+      propsDescription: {
+        size: "Options: 'sm', 'md', 'lg'",
+        variant:
+          "Options: 'primary', 'primary-inverse', 'secondary', 'secondary-inverse', 'back'"
+      }
+    }))
+  )
+
+  .addDecorator(withOptions)
+  .addParameters({ options: { selectedAddonPanel: 'storybook/actions/actions-panel' } })
+  .add("Button Sizes", () => ({
+    components: { ...Components },
+    template: `
+      <div>
+        <hit-button @click="action('Small Button')" size="sm">
+          Small Button
+        </hit-button>
+        
+        <hit-button @click="action('Medium Button')" size="md">
+          Medium Button
+        </hit-button>
+        
+        <hit-button @click="action('Large Button')" size="lg">
+          Large Button
+        </hit-button>
+      </div>
+    `,
+    methods: { action: action("Clicked") }
+  }))
+
+  .addDecorator(withOptions)
+  .addParameters({ options: { selectedAddonPanel: 'storybook/actions/actions-panel' } })
+  .add("Button Variants", () => ({
+      components: { ...Components },
+      template: `
+        <div>
+          <hit-button @click="action" variant="primary">
+            Primary Button
+          </hit-button>
+          
+          <hit-button @click="action" variant="primary-inverse">
+            Primary Inverse Button
+          </hit-button>
+          
+          <hit-button @click="action" variant="secondary">
+            Secondary Button
+          </hit-button>
+          
+          <hit-button @click="action" variant="secondary-inverse">
+            Secondary Inverse Button
+          </hit-button>
+          
+          <hit-button @click="action" variant="back">
+            Back Button
+          </hit-button>
+        </div>
+      `,
+      methods: { action: action("Clicked") }
+  }));
