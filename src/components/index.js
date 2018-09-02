a1e096198125f2133bc535fa651d59f7188e3a46
Gabriel Mule <gabemule@gmail.com>
2018-09-02 00:46:24 -0300

    Adding components wrapper

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import Vue from 'vue';
+
+import hitAlert from "./Alert.vue";
+import hitButton from "./Button.vue";
+
+const Components = {
+  hitAlert,
+  hitButton
+};
+
+export default Components;
